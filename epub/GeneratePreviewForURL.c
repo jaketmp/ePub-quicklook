bd7ad94f59d74364e3d55a1d50bc8a2c473f5311
Jake TM Pearce <jtpearce@ds-jkn30.saf.ic.ac.uk>
2011-04-12 14:20:08 +0100

    Initial Commit

---
+++ epub/GeneratePreviewForURL.c
@@ -0,0 +1,20 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
+
+/* -----------------------------------------------------------------------------
+   Generate a preview for file
+
+   This function's job is to create preview for designated file
+   ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+{
+    #warning To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    return noErr;
+}
+
+void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
+{
+    // implement only if supported
+}
