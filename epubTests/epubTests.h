5c95eb1e06ed8e404d2d94947e181f8916c288d4
Jake TM Pearce <jake.pearce+git@gmail.com>
2012-02-01 18:04:20 +0000

    Added unit-testing framework.
    (empty so far)

---
+++ epubTests/epubTests.h
@@ -0,0 +1,13 @@
+//
+//  epubTests.h
+//  epubTests
+//
+//  Created by Jake Pearce on 01/02/2012.
+//  Copyright (c) 2012 Imperial College. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface epubTests : SenTestCase
+
+@end
---
+++ epubTests/epubTests.m
@@ -0,0 +1,32 @@
+//
+//  epubTests.m
+//  epubTests
+//
+//  Created by Jake Pearce on 01/02/2012.
+//  Copyright (c) 2012 Imperial College. All rights reserved.
+//
+
+#import "epubTests.h"
+
+@implementation epubTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in epubTests");
+}
+
+@end
