fd278feff0e4fcd1280cf498713764a22d89116e
Jake TM Pearce <jake.pearce+git@gmail.com>
2012-02-18 10:54:49 +0000

    Major project restructuring to accommodate a spotlight importer.
    Addressing Issue #17, separated out the quicklook-exclusive files from
    the generic epub handling code, added a target for a mdimporter, which
    as yet does nothing.

---
+++ epubMDI/MySpotlightImporter.h
@@ -0,0 +1,19 @@
+//
+//  MySpotlightImporter.h
+//  epubMDI
+//
+//  Created by Jake Pearce on 18/02/2012.
+//  Copyright (c) 2012 Imperial College. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MySpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
