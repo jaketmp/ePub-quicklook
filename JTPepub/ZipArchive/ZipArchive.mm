708d271f042c5cd7974102127a27281f6e1a391c
Chris Ridd <chrisridd@mac.com>
2012-02-19 16:59:55 +0000

    Use mmap-based I/O instead of stdio
    This is a little faster on small epubs. On bigger files - 70MB, 300MB -
    this is about twice the speed. File sizes seem to be increasing with
    lots of multimedia in ebooks, so this is probably a useful change.
    
    I benchmarked this using "qlmanage -p -z" on a number of files.
    
    A major omission from this code is that writing is not supported.
    
    The unit tests still pass.

--- JTPepub/ZipArchive/ZipArchive.mm
+++ JTPepub/ZipArchive/ZipArchive.mm
@@ -19,7 +19,89 @@
 
 @end
 
+/*
+ * All I/O in unzip is performed using a number of custom functions which can
+ * be overridden. The default functions use stdio.
+ * This implementation uses NSData to mmap the data into memory.
+ *
+ * The "stream" is the NSData object.
+ * The "opaque" value is a pointer to the current file offset.
+ */
+void *mmap_zopen(void *, const char *, int);
+int mmap_zclose(void *, void *);
+long mmap_ztell(void *, void *);
+long mmap_zseek(void *, void *, unsigned long, int);
+int mmap_zerror(void *, void *);
+unsigned long mmap_zread(void *, void *, void *, unsigned long);
 
+// NB this ignores the mode flag and opens the file read-only.
+void *mmap_zopen(void *opaque, const char *filename, int mode)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    NSError *error;
+    NSData *d = [[NSData alloc] initWithContentsOfFile:[NSString stringWithUTF8String:filename]
+                                               options:NSDataReadingMapped
+                                                 error:&error];
+    *pos = 0;
+    return (void *)d;
+}
+
+int mmap_zclose(void *opaque, void *stream)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    NSData *d = (NSData *)stream;
+    *pos = 0;
+    [d release];
+    return Z_OK;
+}
+
+long mmap_ztell(void *opaque, void *stream)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    long ret = (long)*pos;
+    return ret;
+}
+
+long mmap_zseek(void *opaque, void *stream, unsigned long offset, int origin)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    NSData *d = (NSData *)stream;
+    NSInteger newpos;
+    switch (origin) {
+        case ZLIB_FILEFUNC_SEEK_CUR:
+            newpos = *pos + offset;
+            break;
+        case ZLIB_FILEFUNC_SEEK_END:
+            newpos = [d length] - offset;
+            break;
+        case ZLIB_FILEFUNC_SEEK_SET:
+            newpos = offset;
+            break;
+        default:
+            return -1;
+    }
+    *pos = newpos;
+    return 0;
+}
+
+int mmap_zerror(void *opaque, void *stream)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    NSData *d = (NSData *)stream;
+    if (*pos < 0 || *pos > [d length])
+        return -1;
+    return 0;
+}
+
+unsigned long mmap_zread(void *opaque, void *stream, void *buf, unsigned long size)
+{
+    NSInteger *pos = (NSInteger *)opaque;
+    NSData *d = (NSData *)stream;
+    const unsigned char *bytes = (const unsigned char *)[d bytes] + *pos;
+    memcpy(buf, bytes, size);
+    *pos += size;
+    return size;
+}
 
 @implementation ZipArchive
 
@@ -30,9 +112,18 @@
 	{
 		_zipFile = nil ;
         archiveName = nil;
+        mmap_defs.zopen_file = mmap_zopen;
+        mmap_defs.zread_file = mmap_zread;
+        mmap_defs.zwrite_file = NULL; // no writing!
+        mmap_defs.ztell_file = mmap_ztell;
+        mmap_defs.zseek_file = mmap_zseek;
+        mmap_defs.zclose_file = mmap_zclose;
+        mmap_defs.zerror_file = mmap_zerror;
+        mmap_defs.opaque = &(self->pos);
 	}
 	return self;
 }
+
 -(id) initWithZipFile:(NSString *)fileName {
     self = [self init];
     
@@ -40,7 +131,7 @@
     
     [fileName retain];
     
-    _zipFile = unzOpen([fileName fileSystemRepresentation]);
+    _zipFile = unzOpen2([fileName fileSystemRepresentation], &(self->mmap_defs));
     if(_zipFile) {
         return self;
     }else{
