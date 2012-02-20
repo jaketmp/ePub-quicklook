c3e867d5e8398e1d4b6eee8c478defdd7832f042
Chris Ridd <chrisridd@mac.com>
2012-02-20 21:39:43 +0000

    Start a simple Spotlight importer
    Most of the contents of the Apple template files is for Core Data
    projects, so can be deleted. Ultimately all we need is for
    -importFileAtPath:attributes:error: to fill in the attributes array with
    whatever we can extract.
    
    This starts by extracting a few obvious things. I had a look through the
    Spotlight Metadata Attributes document to try and relate them to epub
    fields. That document may not include all known attributes. Should check
    the header files instead!
    
    But, the resulting importer seems to work.
    
    However mdls shows that kMDItemContributors looks odd:
    
    kMDItemContributors            = (
        "Test Author",
        aut
    )
    
    I couldn't see kMDItemDueDate getting filled in either.
    
    We may need custom schema to address epub-only fields like Translator
    et al.

--- epubMDI/MySpotlightImporter.m
+++ epubMDI/MySpotlightImporter.m
@@ -7,153 +7,41 @@
 //
 
 #import "MySpotlightImporter.h"
-
-#define YOUR_STORE_TYPE NSXMLStoreType
-
-@interface MySpotlightImporter ()
-@property (nonatomic, strong) NSURL *modelURL;
-@property (nonatomic, strong) NSURL *storeURL;
-@end
+#import "JTPepub.h"
 
 @implementation MySpotlightImporter
 
-@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
-@synthesize managedObjectModel = __managedObjectModel;
-@synthesize managedObjectContext = __managedObjectContext;
-@synthesize modelURL = _modelURL;
-@synthesize storeURL = _storeURL;
-
-- (void)dealloc
-{
-    [__managedObjectContext release];
-    [__persistentStoreCoordinator release];
-    [__managedObjectModel release];
-    [_modelURL release];
-    [_storeURL release];
-    [super dealloc];
-}
-
 - (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)spotlightData error:(NSError **)error
 {
-        
-    NSDictionary *pathInfo = [NSPersistentStoreCoordinator elementsDerivedFromExternalRecordURL:[NSURL fileURLWithPath:filePath]];
-            
-    self.modelURL = [NSURL fileURLWithPath:[pathInfo valueForKey:NSModelPathKey]];
-    self.storeURL = [NSURL fileURLWithPath:[pathInfo valueForKey:NSStorePathKey]];
+    JTPepub *epub = [[JTPepub alloc] initWithFile:filePath];
 
+    NSString *title = [epub title];
+    [spotlightData setObject:title forKey:(NSString *)kMDItemDisplayName];
 
-    NSURL *objectURI = [pathInfo valueForKey:NSObjectURIKey];
-    NSManagedObjectID *oid = [[self persistentStoreCoordinator] managedObjectIDForURIRepresentation:objectURI];
+    NSArray *authors = [epub authors];
+    [spotlightData setObject:authors forKey:(NSString *)kMDItemAuthors];
 
-    if (!oid) {
-        NSLog(@"%@:%@ to find object id from path %@", [self class], NSStringFromSelector(_cmd), filePath);
-        return NO;
-    }
+    NSString *publisher = [epub publisher];
+    if ([publisher length] > 0)
+        [spotlightData setObject:publisher forKey:(NSString *)kMDItemPublishers];
 
-    NSManagedObject *instance = [[self managedObjectContext] objectWithID:oid];
+    NSArray *creators = [epub creators];
+    if ([creators count] > 0)
+        [spotlightData setObject:creators forKey:(NSString *)kMDItemContributors];
 
-    // how you process each instance will depend on the entity that the instance belongs to
+    // editors
+    // illustrators
+    // translators
+    // synopsis         kMDItemHeadline ?
+    // ISBN             kMDItemIdentifier
+    // publicationDate  kMDItemContentCreationDate
+    // drm              kMDItemSecurityMethod ?
 
-    if ([[[instance entity] name] isEqualToString:@"YOUR_ENTITY_NAME"]) {
-
-        // set the display name for Spotlight search result
-
-        NSString *yourDisplayString =  [NSString stringWithFormat:@"YOUR_DISPLAY_STRING %@", [instance valueForKey:@"SOME_KEY"]];
-        [spotlightData setObject:yourDisplayString forKey:(NSString *)kMDItemDisplayName];
-        
-         /*
-            Determine how you want to store the instance information in 'spotlightData' dictionary.
-            For each property, pick the key kMDItem... from MDItem.h that best fits its content.  
-            If appropriate, aggregate the values of multiple properties before setting them in the dictionary.
-            For relationships, you may want to flatten values. 
-
-            id YOUR_FIELD_VALUE = [instance valueForKey:ATTRIBUTE_NAME];
-            [spotlightData setObject:YOUR_FIELD_VALUE forKey:(NSString *) kMDItem...];
-            ... more property values;
-            To determine if a property should be indexed, call isIndexedBySpotlight
-         */
-    }
+    NSDate *expiryDate = [epub expiryDate];
+    if (expiryDate)
+        [spotlightData setObject:expiryDate forKey:(NSString *)kMDItemDueDate];
 
     return YES;
-}
-
-static NSURL				*cachedModelURL = nil;
-static NSManagedObjectModel *cachedModel = nil;
-static NSDate				*cachedModelModificationDate =nil;
-
-// Returns the managed object model. The last read model is cached in a global variable and reused if the URL and modification date are identical
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (__managedObjectModel != nil)
-        return __managedObjectModel;
-	
-	NSDictionary *modelFileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:[self.modelURL path] error:nil];
-	NSDate *modelModificationDate =  [modelFileAttributes objectForKey:NSFileModificationDate];
-	
-	if ([cachedModelURL isEqual:self.modelURL] && [modelModificationDate isEqualToDate:cachedModelModificationDate]) {
-		__managedObjectModel = [cachedModel retain];
-	} 	
-	
-	if (!__managedObjectModel) {
-		__managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:self.modelURL];
-
-		if (!__managedObjectModel) {
-			NSLog(@"%@:%@ unable to load model at URL %@", [self class], NSStringFromSelector(_cmd), self.modelURL);
-			return nil;
-		}
-
-		// Clear out all custom classes used by the model to avoid having to link them
-		// with the importer. Remove this code if you need to access your custom logic.
-		NSString *managedObjectClassName = [NSManagedObject className];
-		for (NSEntityDescription *entity in __managedObjectModel) {
-			[entity setManagedObjectClassName:managedObjectClassName];
-		}
-		
-		// cache last loaded model
-
-		[cachedModelURL release];
-		cachedModelURL = [self.modelURL retain];
-		[cachedModel release];
-		cachedModel = [__managedObjectModel retain];
-		[cachedModelModificationDate release];
-		cachedModelModificationDate = [modelModificationDate retain];
-	}
-	
-	return __managedObjectModel;
-}
-
-// Returns the persistent store coordinator for the importer.  
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (__persistentStoreCoordinator)
-        return __persistentStoreCoordinator;
-
-    NSError *error = nil;
-        
-    __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![__persistentStoreCoordinator addPersistentStoreWithType:YOUR_STORE_TYPE configuration:nil URL:self.storeURL options:nil error:&error]) {
-        NSLog(@"%@:%@ unable to add persistent store coordinator - %@", [self class], NSStringFromSelector(_cmd), error);
-    }    
-
-    return __persistentStoreCoordinator;
-}
-
-// Returns the managed object context for the importer; already bound to the persistent store coordinator. 
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if (__managedObjectContext)
-        return __managedObjectContext;
-
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-	if (!coordinator) {
-        NSLog(@"%@:%@ unable to get persistent store coordinator", [self class], NSStringFromSelector(_cmd));
-		return nil;
-	}
-
-	__managedObjectContext = [[NSManagedObjectContext alloc] init];
-	[__managedObjectContext setPersistentStoreCoordinator:coordinator];
-    
-    return __managedObjectContext;
 }
 
 @end
